7d21622a8a7677e266942952687943ddb1a88ea4
Tyler Schumacher <tschuma3@uccs.edu>
2023-02-08 21:23:32 -0700

    Fixed 'from ssd' to 'from SSD'

--- Object Detection/ObjectDetection.py
+++ Object Detection/ObjectDetection.py
@@ -2,7 +2,7 @@ import torch
 from torch.autograd import Variable
 import cv2
 from data import BaseTransform, VOC_CLASSES as labelmap
-from ssd import build_ssd
+from SSD import build_ssd
 import imageio
 
 # Defining a function that will do the detections
